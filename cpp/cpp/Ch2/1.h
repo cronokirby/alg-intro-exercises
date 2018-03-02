bf4513bcb64cb49c0bf645961d62bbaf2b9e1825
Lúcás Meier <cronokirby@gmail.com>
2018-03-02 21:28:29 +0100

    removes cpp code

--- cpp/cpp/Ch2/1.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void incInsertionSort(int arr[], size_t size);
-void decInsertionSort(int arr[], size_t size);
-int linearSearch(int v, int arr[], size_t size);
-bool* addBin(bool a[], bool b[], size_t size);
-void merge_sort(int arr[], size_t size);
